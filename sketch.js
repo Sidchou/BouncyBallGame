36220791c4e69138be80a90e5476e6ee359063a8
Sid <sidchou93@gmail.com>
2019-04-03 15:11:39 -0400

    reconfigure

--- sketch.js
+++ sketch.js
@@ -146,7 +146,7 @@ function mousePressed() {
   let cur = createVector(mouseX, mouseY);
   mouseClick.push(cur.copy());
 
-  cur.sub(obj.pos).normalize().mult(-25);
+  cur.sub(obj.pos).normalize().mult(-40);
   obj.applyForce(cur);
   // moves--}
   mouseClickColor.push(60);
